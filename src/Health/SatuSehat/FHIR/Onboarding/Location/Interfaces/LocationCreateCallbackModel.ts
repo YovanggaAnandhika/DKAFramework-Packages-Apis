a186a5c929db30b7a5dc6fb34c8b18eec996dde8
Yovangga Anandhika <dka.tech.dev@gmail.com>
2024-05-15 11:39:46 +0800

    test Commit Sub Module

--- src/Health/SatuSehat/FHIR/Onboarding/Location/Interfaces/LocationCreateCallbackModel.ts
+++ src/Health/SatuSehat/FHIR/Onboarding/Location/Interfaces/LocationCreateCallbackModel.ts
@@ -2,72 +2,72 @@
  * ApidogModel
  */
 export interface LocationCreateCallbackModel {
-    address: LocationCreateCallbackModelAddress;
-    description: string;
-    id: string;
-    identifier: LocationCreateCallbackModelIdentifier[];
-    managingOrganization: LocationCreateCallbackModelManagingOrganization;
-    meta: LocationCreateCallbackModelMeta;
-    mode: string;
-    name: string;
-    physicalType: LocationCreateCallbackModelPhysicalType;
-    position: LocationCreateCallbackModelPosition;
-    resourceType: string;
-    status: string;
-    telecom: LocationCreateCallbackModelTelecom[];
+  address: LocationCreateCallbackModelAddress;
+  description: string;
+  id: string;
+  identifier: LocationCreateCallbackModelIdentifier[];
+  managingOrganization: LocationCreateCallbackModelManagingOrganization;
+  meta: LocationCreateCallbackModelMeta;
+  mode: string;
+  name: string;
+  physicalType: LocationCreateCallbackModelPhysicalType;
+  position: LocationCreateCallbackModelPosition;
+  resourceType: string;
+  status: string;
+  telecom: LocationCreateCallbackModelTelecom[];
 }
 
 export interface LocationCreateCallbackModelAddress {
-    city: string;
-    country: string;
-    extension: LocationCreateCallbackModelAddressExtension[];
-    line: string[];
-    postalCode: string;
-    use: string;
+  city: string;
+  country: string;
+  extension: LocationCreateCallbackModelAddressExtension[];
+  line: string[];
+  postalCode: string;
+  use: string;
 }
 
 export interface LocationCreateCallbackModelAddressExtension {
-    extension?: LocationCreateCallbackModelExtensionExtension[];
-    url?: string;
+  extension?: LocationCreateCallbackModelExtensionExtension[];
+  url?: string;
 }
 
 export interface LocationCreateCallbackModelExtensionExtension {
-    url: string;
-    valueCode: string;
+  url: string;
+  valueCode: string;
 }
 
 export interface LocationCreateCallbackModelIdentifier {
-    system?: string;
-    value?: string;
+  system?: string;
+  value?: string;
 }
 
 export interface LocationCreateCallbackModelManagingOrganization {
-    reference: string;
+  reference: string;
 }
 
 export interface LocationCreateCallbackModelMeta {
-    lastUpdated: string;
-    versionId: string;
+  lastUpdated: string;
+  versionId: string;
 }
 
 export interface LocationCreateCallbackModelPhysicalType {
-    coding: LocationCreateCallbackModelCoding[];
+  coding: LocationCreateCallbackModelCoding[];
 }
 
 export interface LocationCreateCallbackModelCoding {
-    code?: string;
-    display?: string;
-    system?: string;
+  code?: string;
+  display?: string;
+  system?: string;
 }
 
 export interface LocationCreateCallbackModelPosition {
-    altitude: number;
-    latitude: number;
-    longitude: number;
+  altitude: number;
+  latitude: number;
+  longitude: number;
 }
 
 export interface LocationCreateCallbackModelTelecom {
-    system: string;
-    use: string;
-    value: string;
+  system: string;
+  use: string;
+  value: string;
 }
--- src/Health/SatuSehat/FHIR/Onboarding/Location/index.ts
+++ src/Health/SatuSehat/FHIR/Onboarding/Location/index.ts
@@ -1,173 +1,189 @@
-import SatuSehatHostType from "../../../Interfaces/SatuSehatHost.type";
-import {SatuSehatConstructorConfig} from "../../../Interfaces/SatuSehatConstructor.type";
-import {DefaultContructorConfig} from "../../../Config";
-import {SatuSehatFunctionClassParsing} from "../../../Interfaces/SatuSehatFunctionClassParsing";
-import {SatuSehatCallbackProduction} from "../../../Interfaces/SatuSehatCallback.type";
-import {LocationCreateModel} from "./Interfaces/LocationCreateModel";
-import {LocationCreateCallbackModel} from "./Interfaces/LocationCreateCallbackModel";
-import axios from "axios";
-import {LocationReadCallbackModel} from "./Interfaces/LocationReadCallbackModel";
-import {LocationReadModel} from "./Interfaces/LocationReadModel";
-import {LocationUpdateModel} from "./Interfaces/LocationUpdateModel";
-import {LocationUpdateCallback} from "./Interfaces/LocationUpdateCallbackModel";
-
+import SatuSehatHostType from '../../../Interfaces/SatuSehatHost.type';
+import { SatuSehatConstructorConfig } from '../../../Interfaces/SatuSehatConstructor.type';
+import { DefaultContructorConfig } from '../../../Config';
+import { SatuSehatFunctionClassParsing } from '../../../Interfaces/SatuSehatFunctionClassParsing';
+import { SatuSehatCallbackProduction } from '../../../Interfaces/SatuSehatCallback.type';
+import { LocationCreateModel } from './Interfaces/LocationCreateModel';
+import { LocationCreateCallbackModel } from './Interfaces/LocationCreateCallbackModel';
+import axios from 'axios';
+import { LocationReadCallbackModel } from './Interfaces/LocationReadCallbackModel';
+import { LocationReadModel } from './Interfaces/LocationReadModel';
+import { LocationUpdateModel } from './Interfaces/LocationUpdateModel';
+import { LocationUpdateCallback } from './Interfaces/LocationUpdateCallbackModel';
 
 export class LocationClasses {
+  /**
+   * @internal
+   */
+  static hostConfig: SatuSehatHostType | undefined;
+  /**
+   * @internal
+   */
+  static credential: SatuSehatCallbackProduction;
 
-    /**
-     * @internal
-     */
-    static hostConfig: SatuSehatHostType | undefined;
-    /**
-     * @internal
-     */
-    static credential : SatuSehatCallbackProduction;
+  /**
+   *
+   * @internal
+   */
 
-    /**
-     *
-     * @internal
-     */
+  static finalConfig: SatuSehatConstructorConfig = DefaultContructorConfig;
 
-    static finalConfig: SatuSehatConstructorConfig = DefaultContructorConfig;
+  constructor(options: SatuSehatFunctionClassParsing) {
+    LocationClasses.finalConfig = options.config;
+    LocationClasses.hostConfig = options.hostConfig;
+    LocationClasses.credential = options.credential;
+  }
 
-    constructor(options: SatuSehatFunctionClassParsing) {
-        LocationClasses.finalConfig = options.config;
-        LocationClasses.hostConfig = options.hostConfig;
-        LocationClasses.credential = options.credential;
-    }
+  Create(query: LocationCreateModel): Promise<LocationCreateCallbackModel> {
+    return new Promise((resolve, rejected) => {
+      //###########################################################
+      if (LocationClasses.hostConfig === undefined)
+        return rejected({
+          status: false,
+          code: 500,
+          msg: `host config Fatal Error`,
+        });
+      //###########################################################
+      //###########################################################
+      axios<LocationCreateCallbackModel>({
+        url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${LocationClasses.credential.access_token}`,
+          'Content-Type': 'application/json',
+          'Cache-Control': 'no-cache',
+        },
+        data: query,
+      })
+        .then(async (response) => {
+          resolve(response.data);
+        })
+        .catch((error) => {
+          rejected({
+            ...error,
+          });
+        });
+    });
+  }
 
-    Create(query : LocationCreateModel) : Promise<LocationCreateCallbackModel> {
-        return new Promise((resolve, rejected) => {
-            //###########################################################
-            if (LocationClasses.hostConfig === undefined)
-                return rejected({status: false, code: 500, msg: `host config Fatal Error`});
-            //###########################################################
-            //###########################################################
-            axios<LocationCreateCallbackModel>({
-                url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
-                method: "POST",
-                headers: {
-                    Authorization: `Bearer ${LocationClasses.credential.access_token}`,
-                    "Content-Type": "application/json",
-                    "Cache-Control" : "no-cache",
-                },
-                data : query,
-            }).then(async (response) => {
-                resolve(response.data)
-            }).catch((error) => {
-                rejected({
-                    ...error
-                });
+  Read(query: LocationReadModel): Promise<LocationReadCallbackModel> {
+    return new Promise((resolve, rejected) => {
+      //###########################################################
+      if (LocationClasses.hostConfig === undefined)
+        return rejected({
+          status: false,
+          code: 500,
+          msg: `host config Fatal Error`,
+        });
+      //###########################################################
+      switch (query.type) {
+        case 'id':
+          //###########################################################
+          axios<LocationReadCallbackModel>({
+            url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location/${query.id}`,
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${LocationClasses.credential.access_token}`,
+              'Content-Type': 'application/json',
+              'Cache-Control': 'no-cache',
+            },
+          })
+            .then((response) => {
+              resolve(response.data);
+            })
+            .catch((error) => {
+              rejected({
+                ...error,
+              });
             });
-        })
-    }
-
-    Read(query : LocationReadModel) : Promise<LocationReadCallbackModel> {
-        return new Promise((resolve, rejected) => {
-            //###########################################################
-            if (LocationClasses.hostConfig === undefined)
-                return rejected({status: false, code: 500, msg: `host config Fatal Error`});
-            //###########################################################
-            switch (query.type) {
-                case "id":
-                    //###########################################################
-                    axios<LocationReadCallbackModel>({
-                        url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location/${query.id}`,
-                        method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${LocationClasses.credential.access_token}`,
-                            "Content-Type": "application/json",
-                            "Cache-Control" : "no-cache",
-                        }
-                    }).then((response) => {
-                        resolve(response.data)
-                    }).catch((error) => {
-                        rejected({
-                            ...error
-                        });
-                    });
-                    break;
-                case "identifier":
-                    //###########################################################
-                    let mQuery = "";
-                    if (typeof query.identifier === "string") {
-                        mQuery += `http://sys-ids.kemkes.go.id/location/${query.identifier}`;
-                    } else {
-                        let ArrayToString = query.identifier.join("|");
-                        mQuery += `http://sys-ids.kemkes.go.id/location/${ArrayToString}`;
-                    }
-                    axios<LocationReadCallbackModel>({
-                        url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
-                        method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${LocationClasses.credential.access_token}`,
-                            "Content-Type": "application/json",
-                            "Cache-Control" : "no-cache",
-                        },
-                        params : {
-                            identifier : mQuery
-                        }
-                    }).then((response) => {
-                        resolve(response.data)
-                    }).catch((error) => {
-                        rejected({
-                            ...error
-                        });
-                    });
-                    break;
-                case "custom":
-                    //###########################################################
-                    query.type = "custom";
-                    //###########################################################
-                    axios<LocationReadCallbackModel>({
-                        url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
-                        method: "GET",
-                        headers: {
-                            Authorization: `Bearer ${LocationClasses.credential.access_token}`,
-                            "Content-Type": "application/json",
-                            "Cache-Control" : "no-cache",
-                        },
-                        params : {
-                            name : query
-                        }
-                    }).then((response) => {
-                        resolve(response.data)
-                    }).catch((error) => {
-                        rejected({
-                            ...error
-                        });
-                    });
-                    break;
-            }
-
-        })
-    }
-
-
-    Update(query : LocationUpdateModel) : Promise<LocationUpdateCallback> {
-        return new Promise((resolve, rejected) => {
-            //###########################################################
-            if (LocationClasses.hostConfig === undefined)
-                return rejected({status: false, code: 500, msg: `host config Fatal Error`});
-            //###########################################################
-            axios<LocationUpdateCallback>({
-                url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
-                method: "PUT",
-                headers: {
-                    Authorization: `Bearer ${LocationClasses.credential.access_token}`,
-                    "Content-Type": "application/json",
-                    "Cache-Control" : "no-cache",
-                },
-                data : query
-            }).then((response) => {
-                resolve(response.data)
-            }).catch((error) => {
-                rejected({
-                    ...error
-                });
+          break;
+        case 'identifier':
+          //###########################################################
+          let mQuery = '';
+          if (typeof query.identifier === 'string') {
+            mQuery += `http://sys-ids.kemkes.go.id/location/${query.identifier}`;
+          } else {
+            const ArrayToString = query.identifier.join('|');
+            mQuery += `http://sys-ids.kemkes.go.id/location/${ArrayToString}`;
+          }
+          axios<LocationReadCallbackModel>({
+            url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${LocationClasses.credential.access_token}`,
+              'Content-Type': 'application/json',
+              'Cache-Control': 'no-cache',
+            },
+            params: {
+              identifier: mQuery,
+            },
+          })
+            .then((response) => {
+              resolve(response.data);
+            })
+            .catch((error) => {
+              rejected({
+                ...error,
+              });
             });
+          break;
+        case 'custom':
+          //###########################################################
+          query.type = 'custom';
+          //###########################################################
+          axios<LocationReadCallbackModel>({
+            url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
+            method: 'GET',
+            headers: {
+              Authorization: `Bearer ${LocationClasses.credential.access_token}`,
+              'Content-Type': 'application/json',
+              'Cache-Control': 'no-cache',
+            },
+            params: {
+              name: query,
+            },
+          })
+            .then((response) => {
+              resolve(response.data);
+            })
+            .catch((error) => {
+              rejected({
+                ...error,
+              });
+            });
+          break;
+      }
+    });
+  }
 
+  Update(query: LocationUpdateModel): Promise<LocationUpdateCallback> {
+    return new Promise((resolve, rejected) => {
+      //###########################################################
+      if (LocationClasses.hostConfig === undefined)
+        return rejected({
+          status: false,
+          code: 500,
+          msg: `host config Fatal Error`,
+        });
+      //###########################################################
+      axios<LocationUpdateCallback>({
+        url: `${LocationClasses.hostConfig.resources.fhir[LocationClasses.finalConfig.state]}/Location`,
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${LocationClasses.credential.access_token}`,
+          'Content-Type': 'application/json',
+          'Cache-Control': 'no-cache',
+        },
+        data: query,
+      })
+        .then((response) => {
+          resolve(response.data);
         })
-    }
-
+        .catch((error) => {
+          rejected({
+            ...error,
+          });
+        });
+    });
+  }
 }
